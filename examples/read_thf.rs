a70bfbc4a4723fdde654298f0eb02e3930549f63
Paul Duvenage <duvenagep@gmail.com>
2024-10-24 12:00:23 +0200

    feat(edigeo): Add Header

--- examples/read_thf.rs
+++ examples/read_thf.rs
@@ -5,22 +5,37 @@ fn main() {
     let dir = "data/edigeo-740240000A01/";
 
     let e = EdigeoDir::extract_files(dir);
-    // println!("{:#?}", &e);
-
-    // let data = edigeo::read_bytes(&e.geo);
-    // println!("{}", data);
+    println!("{:#?}", &e);
 
     if let Ok(lines) = read_lines_efficient(e.thf) {
-        for line in lines.flatten() {
-            println!("{}", line);
-        }
+        let header = Header::parse_header(lines);
+        println!("{:?}", header);
+        // for line in lines.flatten() {
+        //     println!("{}", line);
+        // }
     }
 
-    // let lines = read_lines(&e.geo);
+    // let l = "BOMT 12:E0000A01.THF";
+    // let (x, _y) = parse_header(l);
 
-    // let header = lines.get(0).unwrap();
-    // println!("{:?}", header);
+    // println!("{:?}", x);
+}
 
-    // let thf = edigeo::read_bytes(&e.thf);
-    // println!("{}", thf);
+#[derive(Debug, Default)]
+pub struct Header {
+    code: Vec<String>,
+    value_type: Vec<String>,
+    value_format: Vec<String>,
+    value_size: Vec<String>,
+}
+
+impl Header {
+    pub fn parse_header(line: Vec<String>) -> Self {
+        Self {
+            code: line[0..3].to_vec(),
+            value_type: line[3..4].to_vec(),
+            value_format: line[4..5].to_vec(),
+            value_size: line[5..7].to_vec(),
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -16,50 +16,25 @@
 pub mod directory;
 use std::{
     fs::{self},
-    io::{self, BufRead, Read},
+    io::{self, BufRead},
     path::Path,
 };
 
 pub use directory::EdigeoDir;
 
-/// The read function take a Path and read the bytes
-/// converting the bytes with Latin1 encoding and
-/// returns a string
-pub fn read<P: AsRef<Path>>(path: P) -> String {
-    let bytes = fs::read(path).unwrap();
-    let contents = String::from_utf8_lossy(&bytes);
-    contents.into_owned()
-}
-
-pub fn read_lines(filename: &str) -> Vec<String> {
-    fs::read_to_string(filename)
-        .unwrap() // panic on possible file-reading errors
-        .lines() // split the string into an iterator of string slices
-        .map(String::from) // make each slice into a string
-        .collect() // gather them together into a vector
-}
-
-pub fn read_bytes<P: AsRef<Path>>(path: P) -> String {
-    let mut rdr = encoding_rs_io::DecodeReaderBytesBuilder::new()
-        .encoding(Some(encoding_rs::WINDOWS_1252))
-        .build(std::fs::File::open(path.as_ref()).unwrap());
-    let mut string = String::new();
-    // This is guaranteed to never return a UTF-8 decoding error since the
-    // transcoding guarantees that its output is valid UTF-8.
-    rdr.read_to_string(&mut string).unwrap();
-
-    string
-}
-
 /// Efficient txt file reader that passes ownership to File::open() which uses a BufReader thus reduing
 /// internal allocations. The internal bytes are decoded using `WINDOWS_1252` encoding (Latin1)
-pub fn read_lines_efficient<P>(path: P) -> io::Result<impl Iterator<Item = io::Result<String>>>
+pub fn read_lines_efficient<P>(path: P) -> io::Result<Vec<String>>
 where
     P: AsRef<Path>,
 {
     let rdr = encoding_rs_io::DecodeReaderBytesBuilder::new()
         .encoding(Some(encoding_rs::WINDOWS_1252))
         .build(fs::File::open(path.as_ref())?);
-
-    Ok(io::BufReader::new(rdr).lines())
+    let lines = io::BufReader::new(rdr)
+        .lines()
+        .map(|l| l.unwrap())
+        .collect::<Vec<String>>();
+    Ok(lines)
+    // Ok(io::BufReader::new(rdr).lines())
 }
